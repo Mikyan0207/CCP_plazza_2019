477a94fae887fc72e1ed8b2fc351225dcd31f398
Florian Théron <florian.theron@epitech.eu>
2020-04-07 13:28:09 +0200

    Better example

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@
 
 void test(int x, int y)
 {
-    std::cout << "Ceci est un test: " << x << " " << y << std::endl;
+    std::cout << "x + y = " << x + y << std::endl;
 }
 
 int main(void)
 {
-    Plazza::Thread<void> thread(test, 10, 10);
+    Plazza::Thread thread(test, 10, 10);
 
     thread.Wait();
     return 0;
